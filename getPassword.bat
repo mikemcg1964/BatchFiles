48b900e75924c2213b8e4ace0196cdf825c68fcb
mikemcg1964 <66666745+mikemcg1964@users.noreply.github.com>
2023-04-11 12:56:36 +0100

    Create getPassword.bat

---
+++ getPassword.bat
@@ -0,0 +1,7 @@
+@echo off
+rem this copies password to clipboard so available to paste rather than type
+rem obvs this is not a secure thing to do but is a timesaver when accessing 
+rem multiple remote machines
+echo myPassord| clip
+rem set /p "pw=Press any key to continue . . . "
+rem pause
